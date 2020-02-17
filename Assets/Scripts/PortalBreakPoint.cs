6005a4929029b2bc7ef3e95d9604a023cd1560fc
zion379 <zspynet22@icloud.com>
2020-02-17 01:01:11 -0800

    Finished slam detection and enemy death, enemy death when in contact with player, and detection for when portalis being shot at to be destroyed.

---
+++ Assets/Scripts/PortalBreakPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalBreakPoint : MonoBehaviour
+{
+    public enum PortalPosition {left,middle,right }
+    public PortalPosition currentPortalPosition = PortalPosition.middle;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Bullet")
+        {
+            Debug.Log("Destroy Portal");
+        }
+    }
+}
